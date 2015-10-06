ba83eda91e80fd001b2e38c5204b92df22b7ab0d
Tim Jenness <tim.jenness@gmail.com>
2015-10-06 16:21:28 -0700

    Use print function

--- code/afw_talk.py
+++ code/afw_talk.py
@@ -1,6 +1,7 @@
 """
 An example of some of the classes included in afw.
 """
+from __future__ import print_function
 
 import lsst.afw.image as afwImage
 import lsst.afw.geom as afwGeom
@@ -11,12 +12,12 @@ n_objects = 1000
 box = afwGeom.BoxI(afwGeom.PointI(300, 500), afwGeom.ExtentI(2000, 2048))
 
 im = afwImage.ImageF(box)
-print im.getBBox()
+print(im.getBBox())
 subbox = afwGeom.BoxI(afwGeom.PointI(10, 10), afwGeom.ExtentI(100, 100))
 try:
     im2 = afwImage.ImageF(im, subbox)
 except LengthError:
-    print "Why didn't that work?\nGOTCHA 1: PARENT vs. LOCAL we need to take into account XY0.\n\n\n"
+    print("Why didn't that work?\nGOTCHA 1: PARENT vs. LOCAL we need to take into account XY0.\n\n\n")
     im2 = afwImage.ImageF(im, subbox, afwImage.LOCAL)
 
 import lsst.afw.math as afwMath
@@ -53,8 +54,8 @@ for x, y in zip(x_positions, y_positions):
     try:
         subim += psf_im
     except NotImplementedError:
-        print "Why didn't this work?\nGOTCHA 2: It's because Psf.computeImage() returns an ImageD. "\
-              "You can't add an ImageD and an ImageF.  They have to be the same type.\n\n"
+        print("Why didn't this work?\nGOTCHA 2: It's because Psf.computeImage() returns an ImageD. "\
+              "You can't add an ImageD and an ImageF.  They have to be the same type.\n\n")
         psf_im = psf_im.convertF()
         subim += psf_im
 
@@ -71,7 +72,7 @@ threshold = afwDetect.createThreshold(5., 'stdev')
 fs = afwDetect.FootprintSet(masked_im, threshold, 'DETECTED')
 display.mtv(masked_im)
 display.incrDefaultFrame()
-print "Wait a second.  Why is everything detected?\nGOTCHA 3: We need to subtract the background.\n\n"
+print("Wait a second.  Why is everything detected?\nGOTCHA 3: We need to subtract the background.\n\n")
 
 bctrl = afwMath.BackgroundControl(11, 11)
 bkgd = afwMath.makeBackground(masked_im, bctrl)
@@ -86,23 +87,23 @@ display.incrDefaultFrame()
 
 # image constructors
 
-print "GOTCHA 4: calling constructors with unexpected parameters can lead to confusing errors "+\
+print("GOTCHA 4: calling constructors with unexpected parameters can lead to confusing errors "+\
       "due to the SWIG wrapping.  This can arise when variable have incorrect values.  See this "+\
-      "by running the following.\n\n"
+      "by running the following.\n\n")
 #im = afwImage.ImageF(1, 1, 1, 1)
 
 # numpy arrays from images
 
 im_arr, mask_arr, var_arr = masked_im.getArrays()
-print type(im_arr)
-print im_arr.dtype
+print(type(im_arr))
+print(im_arr.dtype)
 
 # arrays are views
 xy0 = masked_im.getXY0()
 try:
     box = afwGeom.BoxI(xy0.shift(afwGeom.ExtentI(100, 120)), afwGeom.ExtentI(100, 120))
 except ValueError:
-    print "\n\nGOTCHA 5: Watch out for methods that operate in place.\n\n"
+    print("\n\nGOTCHA 5: Watch out for methods that operate in place.\n\n")
     xy0 = masked_im.getXY0()
     xy0.shift(afwGeom.ExtentI(100, 120))
     box = afwGeom.BoxI(xy0, afwGeom.ExtentI(100, 100)) 
